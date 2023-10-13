29d4e3f674d095bea1d5669d687c34e9b57cdfd7
uezo <uezo@uezo.net>
2023-10-13 20:40:57 +0900

    Change env key for OpenAI API key

--- vsslite/__main__.py
+++ vsslite/__main__.py
@@ -2,5 +2,5 @@ import os
 import uvicorn
 from vsslite import VSSLiteServer
 
-vss = VSSLiteServer(os.getenv("API_KEY"), os.getenv("DATA_PATH"))
+vss = VSSLiteServer(os.getenv("OPENAI_APIKEY"), os.getenv("DATA_PATH"))
 uvicorn.run(vss.app, host="0.0.0.0", port=8000)
